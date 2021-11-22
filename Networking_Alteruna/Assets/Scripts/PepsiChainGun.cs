634145e5ce1ec8f64ec0d7a8a69fb4790fc5d25d
TechnOllieG <oliver.lebert@hotmail.com>
2021-11-22 13:29:34 +0100

    Made pepsi gun spawn things on the network

--- Networking_Alteruna/Assets/Scripts/PepsiChainGun.cs
+++ Networking_Alteruna/Assets/Scripts/PepsiChainGun.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(SpawnerSynchronizable))]
 public class PepsiChainGun : PhysicsWeapon
 {
 	public GameObject pepsi;
 
+	private SpawnerSynchronizable _spawner;
+
+	private void Awake()
+	{
+		_spawner = GetComponent<SpawnerSynchronizable>();
+		_spawner.SpawnableObject = pepsi;
+	}
+
 	public override void Shoot()
 	{
 		base.Shoot();
-		GameObject currentBullet = Instantiate(pepsi, weaponOutput.position, Quaternion.LookRotation(weaponOutput.forward, weaponOutput.up));
+		_spawner.Spawn(weaponOutput.position, Quaternion.LookRotation(weaponOutput.forward, weaponOutput.up), Vector3.one);
 	}
 }
