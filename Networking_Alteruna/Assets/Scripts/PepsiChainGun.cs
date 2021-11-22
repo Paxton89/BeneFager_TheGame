d5f642e1e5a7e45eadab3018ab2b15e3ae930073
TechnOllieG <oliver.lebert@hotmail.com>
2021-11-22 12:08:55 +0100

    Pushed shooting

---
+++ Networking_Alteruna/Assets/Scripts/PepsiChainGun.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PepsiChainGun : PhysicsWeapon
+{
+	public GameObject pepsi;
+
+	public override void Shoot()
+	{
+		base.Shoot();
+		GameObject currentBullet = Instantiate(pepsi, weaponOutput.position, Quaternion.LookRotation(weaponOutput.forward, weaponOutput.up));
+	}
+}
