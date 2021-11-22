812fad6041d7465605529a95263c5600316b3472
Paxton89 <89Johan@live.se>
2021-11-22 14:11:46 +0100

    UI stuff

--- Networking_Alteruna/Assets/Scripts/HealthComponent.cs
+++ Networking_Alteruna/Assets/Scripts/HealthComponent.cs
@@ -2,30 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthComponent : MonoBehaviour
 {
 
     public float maxHealth = 100;
     public float currentHealth;
+    public Text hpValue;
 
     private Quaternion deadRotation;
     private void Start()
     {
         deadRotation = new Quaternion(-90,0,0,0);
         currentHealth = maxHealth;
+        hpValue = GameObject.Find("HP-Value").GetComponent<Text>();
     }
     
     public void TakeDamage(float damage)
     {
-        currentHealth -= damage;
+        int newHealth = (int)(currentHealth -= damage);
         Debug.Log("oof! " + gameObject.name + " took " + damage + " points of dmg!" );
+        hpValue.text = newHealth.ToString();
         if (currentHealth <= 0)
         {
             Die();
         }
     }
-    
+
+    private void FixedUpdate()
+    {
+        TakeDamage(1);
+    }
+
     private void Die()
     {
         //Ragdoll here
@@ -37,9 +46,5 @@ public class HealthComponent : MonoBehaviour
     {
         currentHealth = maxHealth;
         GetComponentInParent<PlayerMovementSync>().canMove = true;
-    }
-    private void OnMouseDown()
-    {
-        //TakeDamage(10);
     }
 }
