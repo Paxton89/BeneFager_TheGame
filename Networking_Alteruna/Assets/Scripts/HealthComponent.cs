82681798477e03bb05044b3b9baa6767152cad4f
Paxton89 <89Johan@live.se>
2021-11-22 14:33:10 +0100

    fixed FIND.

--- Networking_Alteruna/Assets/Scripts/HealthComponent.cs
+++ Networking_Alteruna/Assets/Scripts/HealthComponent.cs
@@ -9,21 +9,17 @@ public class HealthComponent : MonoBehaviour
 
     public float maxHealth = 100;
     public float currentHealth;
-    public Text hpValue;
-
-    private Quaternion deadRotation;
+    public GameObject hpValue;
     private void Start()
     {
-        deadRotation = new Quaternion(-90,0,0,0);
         currentHealth = maxHealth;
-        hpValue = GameObject.Find("HP-Value").GetComponent<Text>();
     }
     
     public void TakeDamage(float damage)
     {
         int newHealth = (int)(currentHealth -= damage);
         Debug.Log("oof! " + gameObject.name + " took " + damage + " points of dmg!" );
-        hpValue.text = newHealth.ToString();
+        hpValue.GetComponent<Text>().text = newHealth.ToString();
         if (currentHealth <= 0)
         {
             Die();
